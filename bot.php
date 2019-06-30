462abcdbaac978d02925654eac3132da5ce0ac43
Gerwin Jo <43228839+gerwinjonathan@users.noreply.github.com>
2019-07-01 00:17:59 +0700

    Change token and channel secret

--- bot.php
+++ bot.php
@@ -2,12 +2,12 @@
 
 class Setting {
     public function getChannelAccessToken() {
-        $channelAccessToken = "rIX37JjnpzIUGJmtQ/Ba8j1ol55s3AeW5RnSYEnmCBKYWkIb3x5EJ7yukIdruy4GkJKbG60T7tx12kFGOHVWWPqjW8ymuYVtCJhGVLl06ZpO0tCxD9CPDttkUHSRyq1VoDf69/yoihv9t+o6KCUeuAdB04t89/1O/w1cDnyilFU=";
+        $channelAccessToken = "";
         return $channelAccessToken;
     }
 
     public function getChannelSecret() {
-        $channelSecret = "4aa61850a71d3ccad137b7f2d5d591d6";
+        $channelSecret = "";
         return $channelSecret;
     }
 
